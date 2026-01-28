9905e230898e72e832055142d2375932e8180237
mateus124 <alvesmateusq00@gmail.com>
2026-01-28 16:21:55 -0300

    Feat: hero lp

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,47 @@
+import { IoMdMoon, IoIosArrowRoundForward  } from "react-icons/io";
+import { useEffect, useRef } from "react";
+import { DotLottie } from "@lottiefiles/dotlottie-web";
+import animationUrl from "../../assets/FinanceApp.lottie?url";
+import styles from './Home.module.css';
+
 const Home = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const dotLottie = new DotLottie({
+                canvas: canvasRef.current,
+                src: animationUrl,
+                loop: true,
+                autoplay: true,
+            });
+
+            return () => {
+                dotLottie.destroy();
+            };
+        }
+    }, []);
+
     return (
         <>
-            <p>Hello</p>
+            <header className={styles.header}>
+                <div className={styles.image}><img src="logo.png" alt="Logo GranaChat" /></div>
+                <nav>
+                    <button><IoMdMoon /></button>
+                    <a href="#">Entrar</a>
+                </nav>
+            </header>
+
+            <main className={styles.main}>
+                <div className={styles.content}>
+                    <h1>GranaChat é o jeito  <br/> mais fácil de <span>organizar suas finanças</span></h1>
+                    <p>Registre gastos, acompanhe sua renda e entenda pra onde seu dinheiro está indo, tudo em uma conversa natural.</p>
+                    <a href="#">Testar agora <IoIosArrowRoundForward className={styles.ico}/></a>
+                </div>
+                <div className={styles.anim}>
+                    <canvas ref={canvasRef} style={{ width: '80%', height: '100%' }}></canvas>
+                </div>
+            </main>
         </>
     )
 }
